c88377d128594a024cce6dc004b82b74d1945fc0
olivergate <oliver.kg2@gmail.com>
2018-12-02 11:49:36 +0000

    basic styleesheet created

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <?php
 ?>
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <title>Probability Calculator</title>
 </head>
 <body>
+    <main>
+        <div class="left_column">
+            <div class="inputs">
 
+            </div>
+            <div class="outputs">
+
+            </div>
+        </div>
+        <div class="right_column">
+            <div class="log_book">
+
+            </div>
+        </div>
+    </main>
 </body>
 </html>
---
+++ src/public/Classes/Either.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: academy
+ * Date: 01/12/2018
+ * Time: 18:14
+ */
+
+namespace Classes;
+
+
+class Either
+{
+
+}
