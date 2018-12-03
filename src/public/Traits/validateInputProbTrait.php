b41eb161d362c89ab256f4214b4f33d69fb2a7d9
olivergate <oliver.kg2@gmail.com>
2018-12-03 12:42:47 +0000

    docbloc added

--- src/public/Traits/validateInputProbTrait.php
+++ src/public/Traits/validateInputProbTrait.php
@@ -4,6 +4,11 @@ namespace pub\Traits;
 
 trait validateInputProbTrait
 {
+    /**Function validateInputProbabilities() takes inputs and figures out if they are probabilities between 0 and 1
+     * If they are not it throws an error.
+     *
+     * @param $inputs an array of values.
+     */
     private function validateInputProbabilities($inputs) : void {
         foreach ($inputs as $input) {
             if($input < 0 || $input > 1) {
