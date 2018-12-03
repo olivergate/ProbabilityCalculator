31e7907077608b0dea76362133fd0094d6b3e876
olivergate <oliver.kg2@gmail.com>
2018-12-03 22:46:05 +0000

    deleted Datstamp, monolog autocreaetes it so it is uneccesary

--- src/public/Traits/logArrayTrait.php
+++ src/public/Traits/logArrayTrait.php
@@ -11,7 +11,6 @@ trait logArrayTrait
     public function logArray() : array
     {
         return $this->outputs = [
-            'Date: ' . date('Y/m/d H:i', time()),
             'Function Name: ' . __CLASS__,
             'Input1: ' . $this->inputs[0], 'Input2: ' . $this->inputs[1],
             'Output: ' . $this->calcResult
