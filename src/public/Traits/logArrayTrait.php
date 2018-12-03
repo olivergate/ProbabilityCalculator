29b2f305cbeac1a9a3ad2372a32593fc70810312
olivergate <oliver.kg2@gmail.com>
2018-12-03 12:40:55 +0000

    reformatted to make it more readable and docblocced

--- src/public/Traits/logArrayTrait.php
+++ src/public/Traits/logArrayTrait.php
@@ -4,8 +4,17 @@ namespace pub\Traits;
 
 trait logArrayTrait
 {
+    /** Function logArray() creates an array of information tha tis ready for the logger to log.
+     *
+     * @return array
+     */
     public function logArray() : array
     {
-        return $this->outputs = ['Date' => date('Y/m/d/H/i', time()), 'Function Name: ' => __CLASS__,  'Input1: ' => $this->inputs[0], 'Input2: ' => $this->inputs[1],  'Output: ' => $this->calcResult];
+        return $this->outputs = [
+            'Date' => date('Y/m/d/H/i', time()),
+            'Function Name: ' => __CLASS__,
+            'Input1: ' => $this->inputs[0], 'Input2: ' => $this->inputs[1],
+            'Output: ' => $this->calcResult
+        ];
     }
 }
